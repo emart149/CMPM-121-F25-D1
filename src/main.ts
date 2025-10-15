a454d85e0ec8e65f2ac2c703db8681f25fd2cfb4
emart149 <emart149@ucsc.edu>
2025-10-15 07:41:42 +0000

    added decimal counter variables that stores the counter as an int and as a string
    added deltaTime that records the time between requestionAnimationFrame() funcitons
    
    gatherRate() is callback function for requestionAnimationFrame()  to increment decimalCounter
    
    (Step 4 Complete)

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 //import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
-let counter = 0;
+// deno-lint-ignore no-unused-vars
+const counter = 0;
+let decimalCounter = 0;
+let decimalCounterStr: string;
 
 document.body.innerHTML = `
   <button id="increment">THROW 🍅</button>
@@ -11,19 +14,40 @@ document.body.innerHTML = `
 
 const button = document.getElementById("increment")!;
 const counterElement = document.createElement("div");
+
 counterElement.textContent = "0 Tomatoes Thrown";
 document.body.appendChild(counterElement);
 
 button.addEventListener("click", () => {
-  counter += 1;
-  counterElement.textContent = `${counter} Tomatoes Thrown`;
+  decimalCounter += 1;
+  counterElement.textContent = `${decimalCounterStr} Tomatoes Thrown`;
   //console.log("I have these thingies:", button, counterElement, counter);
 });
 
-// deno-lint-ignore no-unused-vars
-const audienceInterval = setInterval(audienceMember, 1000);
+/*const audienceInterval = setInterval(audienceMember, 1000);
 
 function audienceMember() {
   counter++;
   counterElement.textContent = `${counter} Tomatoes Thrown`;
+}*/
+
+const fpsDisplay = document.createElement("div");
+//fpsDisplay.textContent = `0 FPS`;
+document.body.appendChild(fpsDisplay);
+
+let deltaTime: number = performance.now();
+let lastTime: number = 0;
+
+function gatherRate(clock: number) {
+  deltaTime = clock - lastTime;
+  //fpsDisplay.textContent = `${deltaTime} FPS`;
+  lastTime = performance.now();
+
+  decimalCounter += deltaTime / 1000;
+  decimalCounterStr = decimalCounter.toFixed(2);
+  counterElement.textContent = `${decimalCounterStr} Tomatoes Thrown`;
+  //fpsDisplay.textContent = `${testCounterStr} testCounter`;
+  requestAnimationFrame(gatherRate);
 }
+
+requestAnimationFrame(gatherRate);
