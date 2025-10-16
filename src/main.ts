5dc4c3f588c8c7ea73e5e87ed9a74bea928e2f31
emart149 <emart149@ucsc.edu>
2025-10-16 07:54:33 +0000

    Initiated Counter to start at 0 and not increment automatically
    throwing tomato button initially checks for when player has > 10 tomatoes but after the first time the upgrade is purchased, the gatherRate function constantly checks to enable/disable upgrade button
    
    had to ensure that requestion Animation frame loop is initiated during the first purchase or else subsquent purchases would start a seprate loop that would mess up the counter
    
    (Step 5 Complete)

--- src/main.ts
+++ src/main.ts
@@ -4,23 +4,35 @@ import "./style.css";
 // deno-lint-ignore no-unused-vars
 const counter = 0;
 let decimalCounter = 0;
-let decimalCounterStr: string;
+let decimalCounterStr: string = "0";
+let multiplier: number = 0;
+let upgradeOneActive = false;
 
 document.body.innerHTML = `
   <button id="increment">THROW 🍅</button>
+  <button id="autoIncrement">Upgrade 1: 10 🍅 </button>
+  
 `;
 //<p>Example img: <img src="${exampleIconUrl}" class="icon" /></p>
 //<p>Tomatoes Thrown: <span id="counter">0</span></p>
 
-const button = document.getElementById("increment")!;
 const counterElement = document.createElement("div");
-
 counterElement.textContent = "0 Tomatoes Thrown";
+const button = document.getElementById("increment")!;
 document.body.appendChild(counterElement);
+
+const autoIncrementButton = document.getElementById(
+  "autoIncrement",
+) as HTMLButtonElement;
+autoIncrementButton.disabled = true;
 
 button.addEventListener("click", () => {
   decimalCounter += 1;
+  decimalCounterStr = decimalCounter.toFixed(2);
   counterElement.textContent = `${decimalCounterStr} Tomatoes Thrown`;
+  if (decimalCounter > 9) {
+    autoIncrementButton.disabled = false;
+  }
   //console.log("I have these thingies:", button, counterElement, counter);
 });
 
@@ -31,23 +43,39 @@ function audienceMember() {
   counterElement.textContent = `${counter} Tomatoes Thrown`;
 }*/
 
-const fpsDisplay = document.createElement("div");
-//fpsDisplay.textContent = `0 FPS`;
-document.body.appendChild(fpsDisplay);
-
 let deltaTime: number = performance.now();
 let lastTime: number = 0;
 
 function gatherRate(clock: number) {
+  if (decimalCounter < 10) {
+    autoIncrementButton.disabled = true;
+  } else {
+    autoIncrementButton.disabled = false;
+  }
+  console.log("decCtr: " + decimalCounter);
   deltaTime = clock - lastTime;
-  //fpsDisplay.textContent = `${deltaTime} FPS`;
   lastTime = performance.now();
 
-  decimalCounter += deltaTime / 1000;
+  decimalCounter += (deltaTime / 1000.00000) * multiplier;
+
   decimalCounterStr = decimalCounter.toFixed(2);
   counterElement.textContent = `${decimalCounterStr} Tomatoes Thrown`;
-  //fpsDisplay.textContent = `${testCounterStr} testCounter`;
   requestAnimationFrame(gatherRate);
 }
 
-requestAnimationFrame(gatherRate);
+autoIncrementButton.addEventListener("click", () => {
+  decimalCounter -= 10;
+  multiplier++;
+  lastTime = performance.now();
+  if (!upgradeOneActive) {
+    requestAnimationFrame(gatherRate);
+    upgradeOneActive = true;
+  }
+});
+
+// deno-lint-ignore no-unused-vars
+const audienceInterval = setInterval(displayCounter, 10);
+
+function displayCounter() {
+  counterElement.textContent = `${decimalCounterStr} Tomatoes Thrown`;
+}
